1fbffe13cf47332191f946e0d66c35bf496a3ae4
Manuela <manuelaplanelles@gmail.com>
2026-01-22 17:27:30 +0100

    empezamos hacer la clase libro

---
+++ javabasico/src/main/java/u05_poo/libroApp.java
@@ -0,0 +1,8 @@
+package u05_poo;
+
+public class libroApp {
+    static void main() {
+        libro libro1 = new libro("El Señor de los anillos","J.R.Tolking");
+        libro1.calcularId();
+    }
+}
