1386445360529cfb552736a7e4b8944aefcce8a3
MANUELA\manue <manuelaplanelles@gmail.com>
2026-01-29 16:05:38 +0100

    ejercicios strings

--- javabasico/src/main/java/miprimerjava/StringsEjercicios.java
+++ javabasico/src/main/java/miprimerjava/StringsEjercicios.java
@@ -2,15 +2,15 @@ package miprimerjava;
 
 public class StringsEjercicios {
     /*
-    Cocatena dos cadenas de texto.
+    Concatena dos cadenas de texto.
     Muestra la longitud de una cadena de texto.
-    Muestra el primer y ultimo caracter de un string
-    Convierte a mayusculas y minusculas un string
+    Muestra el primer y último carácter de un string
+    Convierte a mayúsculas y minúsculas un string
     Comprueba si una cadena de texto contiene una palabra concreta.
     Formatea un string con un entero
     Elimina los espacios en blanco al principio y final de un string
     Sustituye todos los espacios en blanco de un string por un guion
-    Comprueba si dos estrings son iguales.
-    Comprueba si dos strings tienen la misma lonngitud.
+    Comprueba si dos strings son iguales.
+    Comprueba si dos strings tienen la misma longitude.
      */
 }
