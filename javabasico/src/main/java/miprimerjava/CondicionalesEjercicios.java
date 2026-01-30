29c5321eb7bbcf1528f5a431aa3fd85fc6d000ce
MANUELA\manue <manuelaplanelles@gmail.com>
2026-01-30 12:28:48 +0100

    ejercicios de practicas swich y if else

--- javabasico/src/main/java/miprimerjava/CondicionalesEjercicios.java
+++ javabasico/src/main/java/miprimerjava/CondicionalesEjercicios.java
@@ -13,4 +13,85 @@ public class CondicionalesEjercicios {
     Crea un programa que diga si una letra es vocal o consonante (char).
     Usa tres variables a, b, c y muestra cuál es el mayor de las tres.
      */
+
+    static void main() {
+        int edad = 15;
+
+        if (edad >= 18){
+            System.out.println("Puedes votar.");
+        }else{
+            System.out.println("Eres menor de edad, no puedes votar.");
+        }
+
+        int num1 = 5;
+        int num2 = 5;
+
+        if (num1 > num2){
+            System.out.println("El numero " + num1 + " es mayor que " + num2);
+        }else if (num1 < num2){
+            System.out.println("El numero " + num2 + " es mayor que " + num1);
+        }else{
+            System.out.println("Los numeros son iguales.");
+        }
+
+        num1 = 0;
+        if (num1 > 0){
+            System.out.println("El " + num1 + " es positivo");
+        } else if (num1 < 0){
+            System.out.println("El " + num1 + " es negativo");
+        }else{
+            System.out.println("El numero es " + num1);
+        }
+
+        num2 = 13;
+
+        if (num2 % 2 == 0){
+            System.out.println("El " + num2 + " es par");
+        }else{
+            System.out.println("El " + num2 + " es impar");
+        }
+
+        num2 = 105;
+        if (num2 >= 1 && num2 <= 100){
+            System.out.println("El " + num2 + " esta entre 0 y 100");
+        }else{
+            System.out.println("El " + num2 + " no esta entre 0 y 100");
+        }
+
+        int dia = 9;
+        switch (dia){
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miercoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sabado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("No es ningún día de la semana.");
+        }
+        var nota = 105;
+        if (nota >= 0 && nota < 50) {
+            System.out.println("Suspenso");
+        } else if (nota >= 50 && nota <= 80) {
+            System.out.println("Aprobado");
+        } else{
+            System.out.println("Sobresaliente");
+        }
+
+    }
 }
