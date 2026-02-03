3b3b56777657cb2080978f73f774be9d84892e4e
MANUELA\manue <manuelaplanelles@gmail.com>
2026-02-03 15:17:43 +0100

    ejercicios y comprobaciones

---
+++ javabasico/src/main/java/miprimerjava/EstructurasEjercicios.java
@@ -0,0 +1,19 @@
+package miprimerjava;
+
+public class EstructurasEjercicios {
+    /*
+    Crea un Array con 5 valores e imprime su longitud.
+    Modifica uno de los valores de Array e imprime el valor del índice antes y después de modificsrlo.
+    Crea un Array vacío.
+    Añade 4 valores al ArrayList y elimina uno a continuación.
+    Crea un HashSet con 2 valores diferentes.
+    Añade un nuevo valor repetido y otro sin repetir al HashSet.
+    Elimina uno de los elementos del HashSet.
+    Crea un HashSet donde la clave sea un nombre y el valor el número de teléfono. Añade tres contactos.
+    Modifica uno de los contactos y elimina otro.
+    Dado un Array, transfórmalo en un ArrayList, a continuación en un HasthSet y finalmente en un HashMap con clave y valor iguales.
+     */
+    static void main() {
+
+    }
+}
