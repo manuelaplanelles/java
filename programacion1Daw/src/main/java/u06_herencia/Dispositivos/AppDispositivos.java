ec0c2d0a4a035ddc8a3c81b96e10178e2e801bf1
Manuela <manuelaplanelles@gmail.com>
2026-02-17 20:40:03 +0100

    Mejora con clases anónimas el proyecto de Dispositivos Inteligentes

--- programacion1Daw/src/main/java/u06_herencia/Dispositivos/AppDispositivos.java
+++ programacion1Daw/src/main/java/u06_herencia/Dispositivos/AppDispositivos.java
@@ -11,11 +11,55 @@ public class AppDispositivos {
         listaDispositivo.add(new Televisor("LG Smart"));
         listaDispositivo.add(new ParlanteInteligente("Alexa"));
 
+
+
+        Dispositivo proyector = new Dispositivo("Optoma"){
+            @Override
+            public void encender() {
+                if (estado){
+                    System.out.println("El proyector ya está encendido.");
+                }else{
+                    System.out.println("Encendiendo proyector con ajuste automático de brillo...");
+                    estado= true;
+                }
+            }
+        };
+        proyector.encender();
+
+        ControlRemoto proyector_sinc = new ControlRemoto() {
+            @Override
+            public void sincronizar() {
+                System.out.println("Sincronizando proyector con control remoto de presentación...");
+            }
+        };
+        proyector_sinc.sincronizar();
+
+        Dispositivo horno_ia = new Dispositivo("Horno ChatGPT") {
+            @Override
+            public void encender() {
+                if (estado){
+                    System.out.println("El horno ya está encendido.");
+                }else{
+                    System.out.println("Calentando horno con ajuste automático de temperatura..");
+                    estado=false;
+                }
+            }
+        };
+        horno_ia.encender();
+
+
+        listaDispositivo.add(proyector);
+        listaDispositivo.add(horno_ia);
+
         for (Dispositivo dispositivo : listaDispositivo){
             dispositivo.encender();
+
             if(dispositivo instanceof ControlRemoto){
                 ((ControlRemoto) dispositivo).sincronizar();
+            }else if(dispositivo.nombre.equals("Optoma")){
+                proyector_sinc.sincronizar();
             }
+
             dispositivo.mostrarEstado();
             dispositivo.apagar();
         }
